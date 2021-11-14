d8afa3b2a8012a76ef767c114c35207da0706bd0
Juninho <code.devjunior@outlook.com.br>
2021-11-14 15:54:57 -0300

    Projeto Landing Page finalizada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Main from "./Main";
+import Conteudo from "./Conteudo";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Main />
+      <Conteudo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Conteudo.js
@@ -0,0 +1,41 @@
+function Conteudo() {
+  return (
+    <div className="conteudo">
+      <div className="center">
+        <div className="conteudo-single">
+          <h3>Titulo da Chamada</h3>
+          <p>
+            Aqui é o meu conteudo de exemplo para o site da danki code no
+            youtube! Espero que você goste:){" "}
+          </p>
+        </div>
+
+        <div className="conteudo-single">
+          <h3>Titulo da Chamada</h3>
+          <p>
+            Aqui é o meu conteudo de exemplo para o site da danki code no
+            youtube! Espero que você goste:){" "}
+          </p>
+        </div>
+
+        <div className="conteudo-single">
+          <h3>Titulo da Chamada</h3>
+          <p>
+            Aqui é o meu conteudo de exemplo para o site da danki code no
+            youtube! Espero que você goste:){" "}
+          </p>
+        </div>
+
+        <div className="conteudo-single">
+          <h3>Titulo da Chamada</h3>
+          <p>
+            Aqui é o meu conteudo de exemplo para o site da danki code no
+            youtube! Espero que você goste:){" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Conteudo;
